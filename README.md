cf97701399e88417ae66d5f0b7a2e9625c837f3d
Newturno <pongkorn_set@hotmail.com>
2021-12-22 11:05:03 +0700

    Add physionet and model

--- README.md
+++ README.md
@@ -30,3 +30,72 @@
     Train loss: 0.61
 
     Valid loss:0.62
+
+### 19/11/2021
+
+4. Convo1D with 300 epoch all subject
+    
+    Train Acc: 66.97
+
+    Valid Acc: 64.11 / 64.88 (max and save weight)
+
+    Train loss:0.6113
+
+    Valud loss:0.6164
+
+### 20/11/2021
+Insert batchnorm2D and dropout to all convolution 
+
+5. Convo1D finetune with 300epoch all subject (use batchnorm2d)
+
+    Train Acc: 54.94%
+
+    Valid Acc: 49.54% / 51.92(max and save weight)
+
+    Train loss: 0.685
+
+    Valid loss: 0.6930
+
+6. Convo1D finetune with 300epoch all subject (use batchnorm1d)
+
+    Train Acc: 57.29%
+
+    Valid Acc: 41.67% / 62.50(max and save weight)
+
+    Train loss: 0.61
+
+    Valid loss: 0.58
+
+### 21/11/2021
+7. Convo2D finetune with 300 epoch all subject(use batchnorm2d and max pooling2d)
+
+    Train Acc: 52.76%
+
+    Valid Acc: 53.37 / 52.76(max and save weight)
+
+    Train loss: 0.69346
+
+    Valid loss: 0.69341
+
+### 22/11/2021
+Adapt temporal and spatial, reduce third convolution in B block
+
+8. Convo2D Newfinetune with 300 epoch all subject(batchnorm2d and max pooling2d)
+    25 125 4
+    Train Acc: 58.26
+
+    Valid Acc: 51.38
+
+    Train loss: 0.66
+
+    Valid loss: 0.69
+
+9. Convo2D normal with 300 epoch all subject (no maxpooling2d)
+
+    Train Acc: 55.35
+
+    Valid Acc: 51.30
+
+    Train loss: 0.67
+
+    Valid loss: 0.69
--- common.py
+++ common.py
@@ -3,11 +3,13 @@ import copy
 import numpy as np
 
 from tqdm import tqdm
-
+from IPython.display import clear_output
 import torch
 import torch.nn.functional as F
 from torch.utils.data import Dataset
 from torch.utils.data.dataset import ConcatDataset as _ConcatDataset  # noqa
+from torch.autograd import Variable
+import torch.cuda as cuda
 
 
 class ConcatDataset(_ConcatDataset):
@@ -55,128 +57,87 @@ class EpochsDataset(Dataset):
         X = torch.as_tensor(X[None, ...])
         return X, y
 
-
-def _do_train(model, loader, optimizer, criterion, device):
-    iter_loss = 0.0
-    train_acc = 0.0
-    # training loop
-    model.train()
-    pbar = tqdm(loader)
-    train_loss = np.zeros(len(loader))
-    accuracy = 0.
-    for idx_batch, (batch_x, batch_y) in enumerate(pbar):
-        
-        optimizer.zero_grad()
-        batch_x = batch_x.to(device=device, dtype=torch.float32)
-        batch_y = batch_y.to(device=device, dtype=torch.int64)
-
-        output = model(batch_x)
-        loss = criterion(output, batch_y)
-        iter_loss += loss.item()
-        loss.backward()
-        optimizer.step()
-        
-        #accuracy
-        _, top_class = output.topk(1, dim=1)
-        top_class = top_class.flatten()
-        accuracy += \
-                torch.sum((batch_y == top_class).to(torch.float32))
-        
-        train_loss[idx_batch] = loss.item()
-        pbar.set_description(
-            desc="avg train loss: {:.2f}".format(
-                np.mean(train_loss[:idx_batch + 1])))
-    accuracy = accuracy / len(loader.dataset) *100
-    print("---  Accuracy Training set : %.2f %%" % accuracy.item(), "\n")
-    return train_loss,accuracy
-
-
-def _validate(model, loader, criterion, device):
-    # validation loop
-    pbar = tqdm(loader)
-    val_loss = np.zeros(len(loader))
-    accuracy = 0.
-    with torch.no_grad():
-        model.eval()
-
-        for idx_batch, (batch_x, batch_y) in enumerate(pbar):
-            batch_x = batch_x.to(device=device, dtype=torch.float32)
-            batch_y = batch_y.to(device=device, dtype=torch.int64)
-            output = model.forward(batch_x)
-
-            loss = criterion(output, batch_y)
-            val_loss[idx_batch] = loss.item()
-
-            _, top_class = output.topk(1, dim=1)
-            top_class = top_class.flatten()
-            # print(top_class.shape, batch_y.shape)
-            accuracy += \
-                torch.sum((batch_y == top_class).to(torch.float32))
-
-            pbar.set_description(
-                desc="avg val loss: {:.2f}".format(
-                    np.mean(val_loss[:idx_batch + 1])))
-
-    accuracy = accuracy / len(loader.dataset) * 100
-    print("---  Accuracy Validation set: %.2f %%" % accuracy.item(), "\n")
-    return np.mean(val_loss),accuracy
-
-
-def train(model, loader_train, loader_valid, optimizer,criterion, n_epochs, patience,
-          device,wname):
-    """Training function
-    Parameters
-    ----------
-    model : instance of nn.Module
-        The model.
-    loader_train : instance of Sampler
-        The generator of EEG samples the model has to train on.
-        It contains n_train samples
-    loader_valid : instance of Sampler
-        The generator of EEG samples the model has to validate on.
-        It contains n_val samples. The validation samples are used to
-        monitor the training process and to perform early stopping
-    optimizer : instance of optimizer
-        The optimizer to use for training.
-    criterion : instance of loss function
-    n_epochs : int
-        The maximum of epochs to run.
-    patience : int
-        The patience parameter, i.e. how long to wait for the
-        validation error to go down.
-    device : str | instance of torch.device
-        The device to train the model on.
-    wname : str | weights to save model.
-    Returns
-    -------
-    best_model : instance of nn.Module
-        The model that lead to the best prediction on the validation
-        dataset.
-    """
+def train(model, loader_train, loader_valid, optimizer,criterion, n_epochs,device,wname):
     # put model on cuda if not already
     device = torch.device(device)
-    # model.to(device)
-    
-    # define criterion
-    #criterion = F.nll_loss
-    #criterion = torch.nn.CrossEntropyLoss()
+
     weights_name = wname
     best_val_loss = + np.infty
     best_model = copy.deepcopy(model)
     best_accracy = 0
-    waiting = 0
-    train_arr = []
-    valid_arr = []
-
+    train_loss = []
+    valid_loss = []
+    train_accuracy = []
+    valid_accuracy = []
+    
     for epoch in range(n_epochs):
         print("\nStarting epoch {} / {}".format(epoch + 1, n_epochs))
-        train_loss,train_acc = _do_train(model, loader_train, optimizer, criterion, device)
-        val_loss,val_acc = _validate(model, loader_valid, criterion, device)
-        train_arr.append(train_loss[0])
-        valid_arr.append(val_loss)
+        #train_loss,train_acc = _do_train(model, loader_train, optimizer, criterion, device)
+        #val_loss,val_acc = _validate(model, loader_valid, criterion, device)
+        iter_loss = 0.0
+        correct = 0
+        iterations = 0
         
+        model.train()
         
-        # model saving
+        for i, (items, classes) in enumerate(loader_train):
+            items = Variable(items)
+            classes = Variable(classes)
+            
+            if cuda.is_available():
+                items = items.to(device=device)
+                classes = classes.to(device=device)
+            
+            optimizer.zero_grad()
+            outputs = model(items)
+            loss = criterion(outputs, classes)
+            #print(outputs)
+            iter_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+            #print(loss)
+            _, predicted = torch.max(outputs.data, 1)
+            correct += (predicted == classes.data).sum()
+            iterations += 1
+
+        train_loss.append(iter_loss/iterations)
+        
+        train_accuracy.append((100 * correct.float() / len(loader_train.dataset)))
+        
+        loss = 0.0
+        correct = 0
+        iterations = 0
+        model.eval()
+        
+        for i, (items, classes) in enumerate(loader_valid):
+            items = Variable(items)
+            classes = Variable(classes)
+            
+            if cuda.is_available():
+                items = items.to(device=device)
+                classes = classes.to(device=device)
+            
+            outputs = model(items)
+            loss += criterion(outputs, classes).item()
+            
+            _, predicted = torch.max(outputs.data, 1)
+            correct += (predicted == classes.data).sum()
+            
+            iterations += 1
+
+        valid_loss.append(loss/iterations)
+        correct_scalar = np.array([correct.clone().cpu()])[0]
+        valid_accuracy.append(correct_scalar / len(loader_valid.dataset) * 100.0)
+
+        epoch_acc = correct.double()/len(loader_valid.dataset)
+        if valid_loss[-1] < best_val_loss and epoch_acc > best_accracy:
+            best_val_loss = valid_loss[-1]
+            best_accracy = epoch_acc
+            best_model_wts = copy.deepcopy(model.state_dict())
+            model.load_state_dict(best_model_wts)
+            torch.save(model.state_dict(), f"./save_weight/{weights_name}-{epoch}-bestacc{best_accracy * 100:.2f}.pth")
+        
+        '''# model saving
         if (np.mean(val_loss) < best_val_loss) and (val_acc > best_accracy):
             print("\nbest val loss {:.4f} -> {:.4f}".format(
                 best_val_loss, np.mean(val_loss)))
@@ -193,6 +154,8 @@ def train(model, loader_train, loader_valid, optimizer,criterion, n_epochs, pati
         if waiting >= patience:
             print("Stop training at epoch {}".format(epoch + 1))
             print("Best val loss : {:.4f}".format(best_val_loss))
-            break
-
-    return best_model,train_arr,valid_arr
+            break'''
+        print ('Epoch %d/%d, Tr Loss: %.4f, Tr Acc: %.4f, Val Loss: %.4f, Val Acc: %.4f'
+               %(epoch+1, n_epochs, train_loss[-1], train_accuracy[-1], valid_loss[-1], valid_accuracy[-1]))
+         
+    return best_model,train_loss,valid_loss,train_accuracy,valid_accuracy
